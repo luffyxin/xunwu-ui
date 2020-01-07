1246db151bcb44a491580221d0846e1e1990dcf5
dengxin <bumonkey@163.com>
2020-01-07 20:43:25 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <el-button type="warning" @click="testAxios" plain>警告按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    testAxios() {
+      let url = "/api/test/corss";
+      this.axios.get(url).then(res => {
+        console.log(res.data);
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
